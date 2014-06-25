97afc3040a7bac0a3a609c86edfe61b8be28ff33
Sherman Leung <sleung@godaddy.com>
2014-06-25 15:51:19 -0700

    First commit. Barebones rails applicaton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Community::Application.config.secret_key_base = '231b75a89239ff6e3a8ee79f12d43f55f6ab5caaee5991b3d41f5e400c5893da29a6a96040fba06c9424cb656112911db07d217a5a097fe68d5807342b10bb38'
