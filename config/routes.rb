b4442ed3ec8843f29836137194f062729ae7c515
Sherman Leung <skleung@stanford.edu>
2014-06-26 20:58:00 -0700

    Fixed sign-in functionality and homepage works now

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Community::Application.routes.draw do
   get "welcome/index"
-  devise_for :diners
+  devise_for :diners #this needs to be at the top of the file.
+
   resources :ingredients
 
   resources :meals
@@ -11,7 +12,7 @@ Community::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'diners#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
