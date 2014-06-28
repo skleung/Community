5aaa881e4b2392b6b9ae3e8035bcfd9ceb4d924f
Kevin Casey <kcasey@godaddy.com>
2014-06-27 23:38:42 -0700

    showing flash

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Community::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'diners#index'
+  root 'meals#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
