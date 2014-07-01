116adecf56f1b09d34875546dbd4ce24b661b191
Kevin Casey <kcasey@godaddy.com>
2014-07-01 14:45:52 -0700

    show ingredients table in meals

---
+++ app/models/diners_meals.rb
@@ -0,0 +1,10 @@
+# == Schema Information
+#
+# Table name: diners_meals
+#
+#  diner_id :integer          not null
+#  meal_id  :integer          not null
+#
+
+class DinersMeals < ActiveRecord::Base
+end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -12,8 +12,17 @@
 #
 
 class Ingredient < ActiveRecord::Base
-	validates :diner_id, :name, :cost, :presence => true
+  validates :diner_id, :name, :cost, :presence => true
 
-	has_and_belongs_to_many :meals
-	belongs_to :diner
+  has_and_belongs_to_many :meals
+  belongs_to :diner
+
+  def pieces
+    # detemines how many 'servings' this ingredient has been split into.
+    DinersMeals.where(meal_id: meals).count
+  end
+
+  def cost_for_number_of_diners(number_of_diners)
+    cost*number_of_diners/pieces
+  end
 end
